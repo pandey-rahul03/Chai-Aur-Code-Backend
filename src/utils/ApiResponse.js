0a59807d040a1325ae28e66001b6f6570d14bfde
pandey-rahul03 <rahulllll.pandey123@gmail.com>
2024-05-10 16:52:14 +0530

    Some new Changes in files

--- src/utils/ApiResponse.js
+++
@@ -1,9 +0,0 @@
-class ApiRespone {
-     constructor(statusCode, message, data = "success"){
-          this.statusCode = statusCode
-          this.data = data
-          this.message = message
-          this.success = statusCode < 400
-     }
-
-}
